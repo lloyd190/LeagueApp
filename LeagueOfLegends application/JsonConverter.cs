d0a1572df1475c21f6a8a9a36c208e0bf272c8a3
LLOYD\LLOYD17 <lkuijs@student.scalda.nl>
2017-03-14 11:53:35 +0100

    A few static methods in the jstl converting class

---
+++ LeagueOfLegends application/JsonConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueOfLegends_application
+{
+   static class JsonConverter
+    {
+        private const string key = "";
+        //change object to whatever you named the class
+        static public object getCurrentChampion()
+        {
+            return null;
+        }
+        static public object getPlayer()
+        {
+            return null;
+        }
+        static public object getChampionById(int id)
+        {
+            return null;
+        }
+        static public object getGameRecommendedItemBuild()
+        {
+            return null;
+        }
+
+
+    }
+}
