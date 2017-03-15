c58d27e68d5062101013bd910a437e0a387d3482
Unknown <lkuijs@student.scalda.nl>
2017-03-15 21:06:38 +0100

    working database method

--- LeagueOfLegends application/DataLayer_Lol.cs
+++ LeagueOfLegends application/DataLayer_Lol.cs
@@ -11,7 +11,7 @@ namespace LeagueOfLegends_application
    static class DataLayer_Lol
     {
         
-        //change object to whatever you named the class
+        //change method to whatever
         static public object getCurrentChampion()
         {
             return null;
@@ -19,17 +19,16 @@ namespace LeagueOfLegends_application
         static public RiotSharp.SummonerEndpoint.Summoner getPlayer(string name)
         {
            var api = RiotApi.GetInstance("RGAPI-2529f152-1b83-4eda-848f-699780609ad4");
-            var summoner = api.GetSummoner(Region.euw, "illumi Trackball");
+            var summoner = (RiotSharp.SummonerEndpoint.Summoner)null;
             try
             {
-
-               
+             summoner = api.GetSummoner(Region.euw, "illumi Trackball");
             }
             catch (RiotSharpException ex)
             {
                 Console.Write(ex.Data);
             }
-            return null;
+            return summoner;
 
         }
         static public object getChampionById(int id)
