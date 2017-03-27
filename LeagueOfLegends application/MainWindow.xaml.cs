0db1ad9d3cd311d9c391cfb1ca9f1d76425e2103
MarcelKenter <marcyretnk@live.nl>
2017-03-27 20:41:04 +0200

    handy league ip's so you dont have to search it up evry time

--- LeagueOfLegends application/MainWindow.xaml.cs
+++ LeagueOfLegends application/MainWindow.xaml.cs
@@ -34,7 +34,12 @@ namespace LeagueOfLegends_application
         RiotSharp.Region r = RiotSharp.Region.na;
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            string a = new Ping().Send("104.160.131.3").RoundtripTime.ToString() + "ms";
+            //NA - 104.160.131.3
+            //EUW - 104.160.141.3
+            //EUNE - 104.160.142.3
+            //OCE - 104.160.156.1
+            //LAN - 104.160.136.3
+            string a = new Ping().Send("104.160.156.1").RoundtripTime.ToString() + "ms";
             DataLayer_Lol h = new DataLayer_Lol();
             
             if (h.getPlayer(n, r) == null)
